00257497e47c46c596fb732493539c28e09afc14
dasbjornsson <david92@bu.edu>
2017-03-29 22:46:45 -0400

    test6

--- testScripts/status_slave.sh
+++
@@ -1,8 +0,0 @@
-
-#### BigTOP Services Status
-GREEN='\033[0;32m'
-NC='\033[0m' # No Color
-printf ">>>> ${GREEN}Apache BigTop Hadoop-HDFS${NC} Services Status\n"
-systemctl -l --type service -all | grep -i hadoop-hdfs-datanode
-printf ">>>> ${GREEN}Apache BigTop HADOOP-YARN${NC} Services Status\n"
-systemctl -l --type service -all | grep -i hadoop-yarn-nodemanager
