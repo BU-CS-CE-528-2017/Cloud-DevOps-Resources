966a81f31786c672f3b5f7b50a1e2f8438cf1c7c
dasbjornsson <david92@bu.edu>
2017-04-26 09:30:18 -0400

    sp_tst

--- testScripts_ubuntu14/spark_test.sh
+++ testScripts_ubuntu14/spark_test.sh
@@ -1,1 +1,1 @@
-spark-submit --class org.apache.spark.examples.SparkPi --master yarn://hadoop-master:7077 --deploy-mode client --driver-memory 4g --executor-memory 4g /usr/lib/spark/lib/spark-examples-*.jar 1000
+spark-submit --class org.apache.spark.examples.SparkPi --master yarn://hadoop-master:7077 --deploy-mode client --driver-memory 1g --executor-memory 1g /usr/lib/spark/lib/spark-examples-*.jar 1000
