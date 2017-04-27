ef4270547ca343c65d27dc6f7708515a56be97eb
dasbjornsson <david92@bu.edu>
2017-04-27 02:06:52 -0400

    Slack

--- testScripts_ubuntu14/slackMSG.sh
+++ testScripts_ubuntu14/slackMSG.sh
@@ -5,7 +5,7 @@ source $DEPLOYMENT_HOME/deployment-properties.sh
 numVMs=$cons3rt_fap_deployment_numMachines
 runID=$cons3rt_deploymentRun_id
 numSlaves=$(($numVMs-1))
-linkToVM=https://www.cons3rt.com/ui/#/runs/runID
+linkToVM=https://www.cons3rt.com/ui/#/runs/$runID
 
-curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"The Hadoop + Slack Cluster has been deployed - 1 master/$numSlaves | $linkToVM\", \"attachments\":[{\"text\":\"$(./haddopReport.sh;./sparkReport.sh)\"}]}" \
+curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"The Hadoop + Slack Cluster has been deployed | 1 master/$numSlaves | $linkToVM\", \"attachments\":[{\"text\":\"$(./hadoopReport.sh;./sparkReport.sh)\"}]}" \
 https://hooks.slack.com/services/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
